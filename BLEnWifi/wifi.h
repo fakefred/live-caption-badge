138551d55b3652a86422cf66a450ef2ade6e56b5
sylin <b07611033@ntu.edu.tw>
2024-12-03 11:05:31 -0500

    files for BLE connection and Wifi connection.

---
+++ BLEnWifi/wifi.h
@@ -0,0 +1,30 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "gatts_table_creat_demo.h"
+#include "esp_gatt_common_api.h"
+
+void intToStr(int num, char *str);
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data);
+void wifi_init(void);
+
+#endif // WIFI_H
