85fa5279b584aca95f4253fbd758d21cbf01cf1c
Frederick Yin <fkfd@fkfd.me>
2024-12-04 00:12:17 -0500

    transcribe: Do not send POST /upload response

--- transcribe/server_esp.py
+++ transcribe/server_esp.py
@@ -173,7 +173,7 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/html;charset=utf-8")
             self.end_headers()
-            self.wfile.write(partialResult.encode("utf-8"))
+            #  self.wfile.write(partialResult.encode("utf-8"))
             self._write_wav(data, 16000, 16, 1)
 
     def do_GET(self):
